946774f54a9c054eba6c6cb94c3a7a8de475186e
wojtek <wojciechreza@gmail.com>
2018-11-03 14:48:34 +0100

    basics poprawki

---
+++ public/javascripts/data/basics.js
@@ -0,0 +1,93 @@
+let data=[{
+    title:"Rook",
+    exerciseDescription:"Capture black pieces",
+    exerciseHint:"",
+    img:"rook.png",
+  },
+  {
+    title:"Bishop",
+    exerciseDescription:"Capture black pieces",
+    exerciseHint:"",
+    description:[
+    ],
+    img:"bishop.png",
+
+  },
+  {
+    title:"Queen",
+    exerciseDescription:"Capture black pieces",
+    exerciseHint:"",
+    description:[],
+    img:"queen.png",
+
+  },{
+      title:"Knight",
+      exerciseDescription:"Capture black pieces",
+      exerciseHint:"",
+      description:[],
+      img:"knight.png",
+  },{
+      title:"King",
+      exerciseDescription:"Capture black pieces",
+      exerciseHint:"",
+      description:[],
+      img:"king.png",
+
+  },{
+      title:"Pawn",
+      exerciseDescription:"Capture black pieces",
+      exerciseHint:"",
+      description:[],
+      img:"pawn.png",
+
+  },{
+      title:"Pawn promotion",
+      exerciseDescription:"",
+      description:[],
+      img:"",
+
+  },{
+      title:"Check",
+      exerciseDescription:"",
+      description:[],
+      img:"check.png",
+
+  },{
+      title:"Check- defend",
+      exerciseDescription:"",
+      description:[],
+      img:"checkdefend.png",
+
+  },{
+      title:"Checkmate",
+      exerciseDescription:"",
+      description:[],
+      img:"checkmate.png",
+      positions:[
+          '6k1/3Q4/6K1/8/8/8/8/8 w - -',
+          '6k1/8/6K1/8/8/8/4R3/8 w - -',
+          '2k5/2B5/2K5/8/2N5/8/8/8 w - -'
+      ]
+  },{
+      title:"Stalemate",
+      exerciseDescription:"",
+      description:[],
+      img:"stalemate.png",
+        positions:[
+            '7k/8/6KP/8/8/8/8/8 w - -',
+            '7k/8/7K/8/8/8/8/7B w - -',
+            '2k5/2B5/2K5/8/2N5/8/8/8 w - -'
+        ]
+  },{
+      title:"Castle",
+      exerciseDescription:"",
+      description:[],
+      img:"castle.png",
+
+  },{
+      title:"En-passant",
+      exerciseDescription:"",
+      description:[],
+      img:"enpassant.png",
+  }
+];
--- public/javascripts/sites/basics.js
+++ public/javascripts/sites/basics.js
@@ -1,11 +1,12 @@
 let chessGame=ChessGame('board');
+let guideAndTips;
 let chapterRewinder=new Vue({
     el:'#left-menu',
     data:{},
     methods: {
         setMode: function (name) {
         },
-        setHandler: function (chapter) {
+        setHandler: function (chapter,dataIndex) {
 
             switch (chapter) {
                 case'b':
@@ -17,7 +18,8 @@ let chapterRewinder=new Vue({
                     chessGame.setMode(oneColorMovesModeWithoutKingControl);
                     break;
                 case'p':
-                    chessGame.setMode(gameMode);
+                    chessGame.setHandler(pawnHandler);
+                    chessGame.setMode(noOverMode);
                     break;
                 case'pPromotion':
                     break;
@@ -29,17 +31,39 @@ let chapterRewinder=new Vue({
                     chessGame.setHandler(checkHandler(false));
                     chessGame.setMode(noOverMode);
                     break;
+                case 'enPassant':
+                    chessGame.setHandler(enPassantHandler);
+                    chessGame.setMode(noOverMode);
+                    break;
 
             }
             chessGame.handlerInit();
+            guideAndTips.setData(data[dataIndex]);
         }
     },
     computed:{}
 });
-let guideAndTips=new Vue({
+guideAndTips=new Vue({
     el:'#guide',
-    data:{},
-    methods:{}
+    data:{
+        title:'',
+        description:[],
+        exerciseDescription:'',
+        exerciseHint:'',
+        img:'',
+        positions:null
+    },
+    methods:{
+        setData:function(data){
+            this.img=data.img;
+            this.title=data.title;
+            if(data.positions!==undefined){
+                this.positions=data.positions;
+            }
+        },getImgPath:function(){
+            return "img/basics/"+this.img;
+        }
+    }
 });
 function checkHandler(isCheckingSide){
     return {
@@ -55,7 +79,6 @@ function checkHandler(isCheckingSide){
                 PositionManipulator.randomMoveWithCheck(chessBoard.chess);
             }
             chessBoard.refresh();
-            console.log(chessBoard.chess.moves());
         },
         update: function (chessBoard, move) {
             this.numberOfMoves++;
@@ -92,31 +115,111 @@ function checkHandler(isCheckingSide){
 }
 
 let pawnHandler={
+    currentMoves:[],
     init:function(chessBoard){
-
+        this.currentMoves=[];
+        let square=this.generatePawn(chessBoard.chess);
+        this.generatePawnsToCapture(chessBoard.chess,square);
+        this.currentMoves=chessBoard.chess.moves({verbose: true}).map(a => a.san);
+        chessBoard.refresh();
     },
     update:function(chessBoard,move){
-
+        this.currentMoves = this.currentMoves.filter(e => e !== move);
+        if(this.currentMoves.length===0)
+            this.init(chessBoard);
+        else{
+            chessBoard.chess.undo();
+            chessBoard.refresh();
+        }
     },generatePawn:function(chess){
-        let nrSquare=Math.floor(Math.random()*32)+8;
+        chess.clear();
+        let factor=Math.floor(Math.random()*8);
+        let line=Math.floor(Math.random()*5)+1;
+        let nrSquare=8*factor+line;
         let square=PositionManipulator.nrToSquare(nrSquare);
-        chess.put(square,{type:'p',color:'w'});
+        chess.put({type:'p',color:'w'},square);
         return square;
-    },generatePawnsToCapture(chess,nrSquare) {
+    },generatePawnsToCapture(chess,square) {
+        let letter=square.charCodeAt(0);
+        let number=parseInt(square.charAt(1))+1;
         let numberOfPawns = Math.floor(Math.random() * 3);
+        let piece1=PositionManipulator.nrToPiece(Math.floor(Math.random()*5));
         if(numberOfPawns===1){
-            let pawnSquare;
             let isLeftSide=Math.floor(Math.random()*2)===0;
-            if(isLeftSide)
-                pawnSquare=nrSquare+7;
-            else
-                pawnSquare=nrSquare+9;
-            chess.put(PositionManipulator.nrToSquare(pawnSquare),{type:'p',color:'b'});
+            letter=letter+(isLeftSide?-1:1);
+            letter=String.fromCharCode(letter);
+            PositionManipulator.setPieces(chess,piece1,'b',[letter+number.toString()]);
         }else if(numberOfPawns===2){
-            chess.put(PositionManipulator.nrToSquare(nrSquare+7),{type:'p',color:'b'});
-            chess.put(PositionManipulator.nrToSquare(nrSquare+9),{type:'p',color:'b'});
+            let l1=letter-1;
+            let l2=letter+1;
+            l1=String.fromCharCode(l1);
+            l2=String.fromCharCode(l2);
+            let s1=l1.toString()+number.toString();
+            let s2=l2.toString()+number.toString();
+            let piece2=PositionManipulator.nrToPiece(Math.floor(Math.random()*5));
+            PositionManipulator.setPieces(chess,piece1,'b',[s1]);
+            PositionManipulator.setPieces(chess,piece2,'b',[s2]);
         }
     }
 };
 
+let enPassantHandler={
+    init:function(chessBoard){
 
+        this.generatePawns(chessBoard.chess);
+        chessBoard.refresh();
+        let handler=this;
+        setTimeout(function(){
+            handler.makeFirstMove(chessBoard.chess);
+            chessBoard.refresh();
+        },500);
+    },
+    update:function(chessBoard){
+        this.init(chessBoard);
+    },
+    generatePawns:function(chess){
+        chess.clear();
+        for(let i=97;i<105;i++){
+           chess.put({type:'p',color:'b'},String.fromCharCode(i)+'7');
+        }
+        for(let i=0;i<4;i++){
+            let letter=String.fromCharCode(Math.floor(Math.random()*8)+97);
+            chess.put({type:'p',color:'w'},letter+'5');
+        }
+        let letter;
+        letter=String.fromCharCode(Math.floor(Math.random()*8)+97);
+        chess.put({type:'p',color:'w'},letter+'4');
+        letter=String.fromCharCode(Math.floor(Math.random()*8)+97);
+        chess.put({type:'p',color:'w'},letter+'6');
+    },
+    makeFirstMove:function(chess) {
+        PositionManipulator.changeSideOnMove(chess);
+        let whiteResponses=[];
+        let longPawnsMoves=chess.moves({verbose:true}).filter(move=> move.flags === 'b');
+        console.log(longPawnsMoves);
+        let move;
+        do{
+            move=longPawnsMoves[Math.floor(Math.random()*longPawnsMoves.length)];
+            chess.move(move);
+
+            whiteResponses=chess.moves({verbose:true}).filter(move=> move.flags === 'e');
+
+            chess.undo();
+        }while(whiteResponses.length === 0 );
+        chess.move(move);
+    }
+};
+
+let positionHandler={
+    positions:[],
+    init:function (chessBoard) {
+        chessBoard.load(this.positions[Math.floor(Math.random()*this.positions.length)]);
+        chessBoard.refresh();
+    },
+    update:function(chessBoard,move){
+        this.init();
+    },
+    setPositions(positions){
+        this.positions=positions;
+    },validator:null
+};
