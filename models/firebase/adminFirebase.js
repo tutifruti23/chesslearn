30b51b73d1ca2fc2898744b42c1c3f7892f1b4e4
Wojciech reza <wojciechreza@gmail.com>
2023-01-02 18:57:12 +0100

    Fixes

--- models/firebase/adminFirebase.js
+++ models/firebase/adminFirebase.js
@@ -2,17 +2,22 @@ let admin = require("firebase-admin");
 exports.FieldValue = admin.firestore.FieldValue;
 let serviceAccount = require("./service2.json");
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://chesstrainer-c8633.firebaseio.com/"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://chesstrainer-c8633.firebaseio.com/",
 });
-exports.admin=admin;
-exports.db=admin.firestore();
-exports.getUserIdFromToken=function(token,callback){
-    admin.auth().verifyIdToken(token)
-        .then(function(decodedToken) {
-            callback( decodedToken.uid)
-        }).catch(function(error) {
-        // Handle error
-    });
+exports.admin = admin;
+const db = admin.firestore();
 
+exports.db = db;
+
+exports.getUserIdFromToken = function (token, callback) {
+  admin
+    .auth()
+    .verifyIdToken(token)
+    .then(function (decodedToken) {
+      callback(decodedToken.uid);
+    })
+    .catch(function (error) {
+      // Handle error
+    });
 };
